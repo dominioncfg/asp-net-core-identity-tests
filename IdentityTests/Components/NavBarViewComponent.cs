0216e3860adc143ae8e3bdc5debfe7802be8c68b
Jose Carlos Nuñez Martinez <josecdom94@gmail.com>
2020-09-24 22:50:50 +0200

    DIsplay Current Role

--- IdentityTests/Components/NavBarViewComponent.cs
+++ IdentityTests/Components/NavBarViewComponent.cs
@@ -1,5 +1,9 @@
-﻿using IdentityTests.ViewModels;
+﻿using IdentityTests.Models;
+using IdentityTests.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Security.Claims;
 
 namespace IdentityTests.Components
 {
@@ -7,10 +11,20 @@ namespace IdentityTests.Components
     {
         public IViewComponentResult Invoke()
         {
+            var userPrincipal = User as ClaimsPrincipal;
+            string strRole = userPrincipal?.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.Role)?.Value;
+
+            Roles role = Roles.RegularUser;
+            if (!string.IsNullOrEmpty(strRole))
+            {
+                bool parsed = Enum.TryParse(strRole, out role);
+            }
+
             NavBarViewModel viewModel = new NavBarViewModel()
             {
                 IsLoggedIn = User.Identity.IsAuthenticated,
-                UserName = User.Identity.Name
+                UserName = User.Identity.Name,
+                CurrentRole = role
             };
             return View(viewModel);
         }
--- IdentityTests/ViewModels/NavBarViewModel.cs
+++ IdentityTests/ViewModels/NavBarViewModel.cs
@@ -1,8 +1,10 @@
-﻿namespace IdentityTests.ViewModels
+﻿using IdentityTests.Models;
+namespace IdentityTests.ViewModels
 {
     public class NavBarViewModel
     {
         public bool IsLoggedIn { get; set; }
         public string UserName { get; set; }
+        public Roles CurrentRole { get; set; }
     }
 }
